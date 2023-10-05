eeb6e2da40620f5364786c7d1efe22ae6f346dd4
Robert <3417357+robert-skarzycki@users.noreply.github.com>
2023-10-05 16:36:29 +0200

    Meeting model

---
+++ src/CalendarAnalyser/CalendarAnalyser.Core/Meeting.cs
@@ -0,0 +1,5 @@
+﻿using System;
+
+namespace CalendarAnalyser.Core;
+
+public record Meeting(string Subject, DateTime StartDateTime, DateTime EndDateTime, int AttendeesCount, string Organizer, bool IsAllDay, bool IsSelfOrganized);
