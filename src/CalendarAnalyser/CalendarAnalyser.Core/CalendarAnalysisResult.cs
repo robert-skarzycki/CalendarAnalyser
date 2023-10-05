5d23db3856065552ad0f924a8953e87d2f42b1b8
Robert <3417357+robert-skarzycki@users.noreply.github.com>
2023-10-05 19:09:02 +0200

    Analysis result as class that calculates percentages

--- src/CalendarAnalyser/CalendarAnalyser.Core/CalendarAnalysisResult.cs
+++ src/CalendarAnalyser/CalendarAnalyser.Core/CalendarAnalysisResult.cs
@@ -1,6 +1,29 @@
-﻿using System;
+﻿using CalendarAnalyser.Core.Configuration;
+using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace CalendarAnalyser.Core;
 
-public record CalendarAnalysisResult(Dictionary<string, TimeSpan> TotalDurationPerCategory);
+public class CalendarAnalysisResult
+{
+    public Dictionary<string, AnalyzedCategoryInfo> Categories { get; } = new Dictionary<string, AnalyzedCategoryInfo>();
+
+    public CalendarAnalysisResult(Dictionary<string, TimeSpan> totalDurationPerCategory, TimeSpan totalWorkingTime)
+    {
+        foreach(var categoryPair in totalDurationPerCategory)
+        {
+            var categoryName = categoryPair.Key;
+            var totalTimeInCategory = categoryPair.Value;
+
+            Categories[categoryName] = new AnalyzedCategoryInfo(totalTimeInCategory, totalTimeInCategory.TotalMinutes / totalWorkingTime.TotalMinutes);
+        }
+
+        var totalFreeTime = totalWorkingTime.TotalMinutes - Categories.Sum(c => c.Value.TotalDuration.TotalMinutes);
+
+        Categories[Constants.FreeCategoryName] = new AnalyzedCategoryInfo(TimeSpan.FromMinutes(totalFreeTime), 1 - Categories.Sum(c => c.Value.Percentage));
+    }
+}
+
+public record AnalyzedCategoryInfo(TimeSpan TotalDuration, double Percentage);
+    
