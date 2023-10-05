cc2dcfc9ef5e90d5f1da27bacb5e4c357d35c851
Robert <3417357+robert-skarzycki@users.noreply.github.com>
2023-10-05 17:36:55 +0200

    Simple RegexAnalysisRule

---
+++ src/CalendarAnalyser/CalendarAnalyser.Core/RegexAnalysisRule.cs
@@ -0,0 +1,21 @@
+﻿using System.Text.RegularExpressions;
+
+namespace CalendarAnalyser.Core;
+
+public class RegexAnalysisRule : IAnalysisRule
+{
+    private readonly Regex regex;
+
+    public RegexAnalysisRule(Regex regex, string categoryName)
+    {
+        this.regex = regex;
+        Category = categoryName;
+    }
+
+    public string Category { get; init; }
+
+    public bool IsMatch(Meeting meeting)
+    {
+        return regex.IsMatch(meeting.Subject);
+    }
+}
