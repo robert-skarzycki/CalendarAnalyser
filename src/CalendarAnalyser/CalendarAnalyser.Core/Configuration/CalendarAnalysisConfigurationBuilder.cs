07cca8165f59d0791e63e4aa1a3ae29e28f83998
Robert <3417357+robert-skarzycki@users.noreply.github.com>
2023-10-05 16:45:29 +0200

    CalendarAnalysisConfigurationBuilder first draft

---
+++ src/CalendarAnalyser/CalendarAnalyser.Core/Configuration/CalendarAnalysisConfigurationBuilder.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CalendarAnalyser.Core.Configuration;
+
+public class CalendarAnalysisConfigurationBuilder 
+{
+    private readonly CalendarAnalysisConfiguration configuration = new();
+
+    public CalendarAnalysisConfigurationBuilder WithTimeResolution(TimeSpan timeResolution)
+    {
+        configuration.TimeResolution = timeResolution;
+        return this;
+    }
+
+    public CalendarAnalysisConfigurationBuilder WithCoreHoursStartAt(TimeSpan coreHoursStartAt)
+    {
+        configuration.CoreHoursStartTime = coreHoursStartAt;
+        return this;
+    }
+
+    public CalendarAnalysisConfigurationBuilder WithCoreHoursEndAt(TimeSpan coreHoursEndAt)
+    {
+        configuration.CoreHoursEndTime = coreHoursEndAt;
+        return this;
+    }
+
+    public CalendarAnalysisConfigurationBuilder WithRules(ICollection<IAnalysisRule> rules)
+    {
+        configuration.Rules = rules;
+        return this;
+    }
+
+    public CalendarAnalysisConfiguration Build()
+    {
+        return configuration;
+    }
+}
