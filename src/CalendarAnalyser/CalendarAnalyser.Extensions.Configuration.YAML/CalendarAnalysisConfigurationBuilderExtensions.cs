27ed8e833a072b4fc0665769a654eb54d6c87da1
Robert <3417357+robert-skarzycki@users.noreply.github.com>
2023-11-02 20:19:36 +0100

    YAMLDeserializer extracted

--- src/CalendarAnalyser/CalendarAnalyser.Extensions.Configuration.YAML/CalendarAnalysisConfigurationBuilderExtensions.cs
+++ src/CalendarAnalyser/CalendarAnalyser.Extensions.Configuration.YAML/CalendarAnalysisConfigurationBuilderExtensions.cs
@@ -1,7 +1,4 @@
-﻿using CalendarAnalyser.Core;
-using CalendarAnalyser.Core.Configuration;
-using System.Text.RegularExpressions;
-using YamlDotNet.Serialization;
+﻿using CalendarAnalyser.Core.Configuration;
 
 namespace CalendarAnalyser.Extensions.Configuration.YAML;
 
@@ -15,38 +12,8 @@ public static class CalendarAnalysisConfigurationBuilderExtensions
         }
         var fileContent = File.ReadAllText(filePath);
 
-        var deserializer = new DeserializerBuilder()
-            .WithTypeDiscriminatingNodeDeserializer(o =>
-            {
-                var valueMappings = new Dictionary<string, Type> 
-                {
-                    { RuleTypes.Regex, typeof(RegexAnalysisRuleDto) }
-                };
-                o.AddKeyValueTypeDiscriminator<AnalysisRuleDto>(nameof(AnalysisRuleDto.Type), valueMappings);
-            })
-            .Build();
-
-        var dto = deserializer.Deserialize<AnalysisMappingDto>(fileContent);
-
-        var rules = MaptDtoToRules(dto);
+        var rules = YAMLDeserializer.Deserialize(fileContent);
 
         return builder.WithRules(rules.ToList());
-    }
-
-    private static IEnumerable<IAnalysisRule> MaptDtoToRules(AnalysisMappingDto dto)
-    {
-        foreach(var category in dto.Categories)
-        {
-            foreach(var rule in category.Rules)
-            {
-                var analysisRule = rule switch
-                {
-                    RegexAnalysisRuleDto regexRule => new RegexAnalysisRule(new Regex(regexRule.Pattern), category.Name),
-                    _ => throw new IndexOutOfRangeException("Invalid rule type")
-                };
-
-                yield return analysisRule;
-            }
-        }
     }
 }
---
+++ src/CalendarAnalyser/CalendarAnalyser.Extensions.Configuration.YAML/YAMLDeserializer.cs
@@ -0,0 +1,47 @@
+﻿using CalendarAnalyser.Core;
+using System.Text.RegularExpressions;
+using YamlDotNet.Serialization;
+using YamlDotNet.Serialization.NamingConventions;
+
+namespace CalendarAnalyser.Extensions.Configuration.YAML;
+
+internal static class YAMLDeserializer
+{
+    public static IEnumerable<IAnalysisRule> Deserialize(string yamlContent)
+    {
+        var deserializer = new DeserializerBuilder()
+            .WithNamingConvention(CamelCaseNamingConvention.Instance)
+            .WithTypeDiscriminatingNodeDeserializer(o =>
+            {
+                var valueMappings = new Dictionary<string, Type>
+                {
+                    { RuleTypes.Regex, typeof(RegexAnalysisRuleDto) }
+                };
+                o.AddKeyValueTypeDiscriminator<AnalysisRuleDto>("type", valueMappings);
+            })
+            .Build();
+
+        var dto = deserializer.Deserialize<AnalysisMappingDto>(yamlContent);
+
+        var rules = MaptDtoToRules(dto);
+
+        return rules;
+    }
+
+    private static IEnumerable<IAnalysisRule> MaptDtoToRules(AnalysisMappingDto dto)
+    {
+        foreach (var category in dto.Categories)
+        {
+            foreach (var rule in category.Rules)
+            {
+                var analysisRule = rule switch
+                {
+                    RegexAnalysisRuleDto regexRule => new RegexAnalysisRule(new Regex(regexRule.Pattern), category.Name),
+                    _ => throw new IndexOutOfRangeException("Invalid rule type")
+                };
+
+                yield return analysisRule;
+            }
+        }
+    }
+}
