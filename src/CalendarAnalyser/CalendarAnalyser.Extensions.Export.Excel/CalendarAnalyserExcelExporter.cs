8bcd428946a536c8c4fa60bacb5da8a4a1934972
Robert <3417357+robert-skarzycki@users.noreply.github.com>
2023-11-07 17:11:49 +0100

    Category stats worksheet creation

--- src/CalendarAnalyser/CalendarAnalyser.Extensions.Export.Excel/CalendarAnalyserExcelExporter.cs
+++ src/CalendarAnalyser/CalendarAnalyser.Extensions.Export.Excel/CalendarAnalyserExcelExporter.cs
@@ -1,12 +1,44 @@
 ﻿using CalendarAnalyser.Core;
+using SpreadCheetah;
 
 namespace CalendarAnalyser.Extensions.Export.Excel
 {
     public static class CalendarAnalyserExcelExporter
     {
-        public async Task Export(string filePath, CalendarAnalysisResult result)
+        public static async Task Export(string filePath, CalendarAnalysisResult result)
         {
+            using var stream = File.Create(filePath);
+            using var spreadsheet = await Spreadsheet.CreateNewAsync(stream);
+            
+            await BuildCategoryStatsWorksheeet(spreadsheet, result);
 
+            await spreadsheet.FinishAsync();
+        }
+
+        private static async Task BuildCategoryStatsWorksheeet(Spreadsheet spreadsheet, CalendarAnalysisResult result)
+        {
+            await spreadsheet.StartWorksheetAsync("Category stats");
+
+            var headerRow = new List<Cell>
+            {
+                new Cell("Category name"),
+                new Cell("Percentage"),
+                new Cell("Total duration")
+            };
+
+            await spreadsheet.AddRowAsync(headerRow);
+
+            foreach (var category in result.CategoriesAnalysis.Categories)
+            {
+                var row = new List<Cell>
+                {
+                    new Cell(category.Key),
+                    new Cell(category.Value.Percentage),
+                    new Cell(category.Value.TotalDuration.ToString())
+                };
+
+                await spreadsheet.AddRowAsync(row);
+            }
         }
     }
 }
