9e73d125ed891fb2967b6ed7b72131b1e130db6d
Robert <3417357+robert-skarzycki@users.noreply.github.com>
2023-11-07 16:53:24 +0100

    Excel export library added

---
+++ src/CalendarAnalyser/CalendarAnalyser.Extensions.Export.Excel/CalendarAnalyserExcelExporter.cs
@@ -0,0 +1,12 @@
+﻿using CalendarAnalyser.Core;
+
+namespace CalendarAnalyser.Extensions.Export.Excel
+{
+    public static class CalendarAnalyserExcelExporter
+    {
+        public async Task Export(string filePath, CalendarAnalysisResult result)
+        {
+
+        }
+    }
+}
