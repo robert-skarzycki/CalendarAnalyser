02224e8253db92a1f69aafdedbc222e6f5f2e36c
Robert <3417357+robert-skarzycki@users.noreply.github.com>
2023-11-08 17:10:14 +0100

    Geenrating CaledarSlot sheets

--- src/CalendarAnalyser/CalendarAnalyser.Extensions.Export.Excel/CalendarAnalyserExcelExporter.cs
+++ src/CalendarAnalyser/CalendarAnalyser.Extensions.Export.Excel/CalendarAnalyserExcelExporter.cs
@@ -12,7 +12,30 @@ namespace CalendarAnalyser.Extensions.Export.Excel
             
             await BuildCategoryStatsWorksheeet(spreadsheet, result);
 
+            await BuildSlotsWorksheet(spreadsheet, result);
+
             await spreadsheet.FinishAsync();
+        }
+
+        private static async Task BuildSlotsWorksheet(Spreadsheet spreadsheet, CalendarAnalysisResult result)
+        {
+            var groupedSlots = result.CalendarSlots.GroupBy(s => s.SlotStartDateTime.Date);
+
+            foreach(var day in groupedSlots) 
+            {
+                await spreadsheet.StartWorksheetAsync(day.Key.ToString("ddd yyyy-MM-dd"));
+
+                foreach(var slot in day)
+                {
+                    var row = new List<Cell>
+                    {
+                        new Cell(slot.SlotStartDateTime.ToString()),
+                        new Cell(slot.Category)
+                    };
+
+                    await spreadsheet.AddRowAsync(row);
+                }
+            }
         }
 
         private static async Task BuildCategoryStatsWorksheeet(Spreadsheet spreadsheet, CalendarAnalysisResult result)
