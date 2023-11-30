bbc503e06daf8be8691ce54ea2a9044ed5b8c90d
Robert <3417357+robert-skarzycki@users.noreply.github.com>
2023-11-30 18:39:25 +0100

    E2E tests for reading rules from YAML file

---
+++ src/CalendarAnalyser/CalendarAnalyser.Extensions.Configuration.YAML.Tests/CalendarAnalysisConfigurationBuilderExtensionsTests.cs
@@ -0,0 +1,16 @@
+﻿using CalendarAnalyser.Core.Configuration;
+
+namespace CalendarAnalyser.Extensions.Configuration.YAML.Tests;
+
+public class CalendarAnalysisConfigurationBuilderExtensionsTests
+{
+    [Fact]
+    public void ReadsRulesFromFile()
+    {
+        var configuration = new CalendarAnalysisConfigurationBuilder()
+            .WithRulesFromFile("test-rules.yaml")
+            .Build();
+
+        configuration.Rules.Should().HaveCount(1);
+    }
+}
