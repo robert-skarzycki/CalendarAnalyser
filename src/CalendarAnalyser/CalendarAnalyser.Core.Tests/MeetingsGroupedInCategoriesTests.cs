d437547b81f032c540cd21fd940ceca9790e89ac
Robert <3417357+robert-skarzycki@users.noreply.github.com>
2023-11-30 18:36:38 +0100

    Unit test for multiple rules with same categories

---
+++ src/CalendarAnalyser/CalendarAnalyser.Core.Tests/MeetingsGroupedInCategoriesTests.cs
@@ -0,0 +1,28 @@
+﻿using CalendarAnalyser.Core.Configuration;
+using FluentAssertions;
+using System.Text.RegularExpressions;
+
+namespace CalendarAnalyser.Core.Tests;
+
+public class MeetingsGroupedInCategoriesTests
+{
+    [Fact]
+    public void DistinctsCategoriesEvenMultipleRulesForSameCategory()
+    {
+        var meetings = new Meeting[0];
+        var DummyCategoryName = "DummyCategory";
+        var configuration = new CalendarAnalysisConfiguration
+        {
+            Rules = new IAnalysisRule[] { 
+                new RegexAnalysisRule(new Regex("a"), DummyCategoryName), 
+                new RegexAnalysisRule(new Regex("b"), DummyCategoryName),
+                new RegexAnalysisRule(new Regex("c"), "OtherCategory")
+            }
+        };
+
+        var sut = new MeetingsGroupedInCategories(meetings, configuration);
+
+        sut.Data.Keys.Should().HaveCount(2);
+        sut.Data.Keys.Contains(DummyCategoryName).Should().BeTrue();
+    }
+}
