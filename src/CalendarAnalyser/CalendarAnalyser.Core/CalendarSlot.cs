4bc8bddaad0de51d58772b70bb2f77dc2b1d4464
Robert <3417357+robert-skarzycki@users.noreply.github.com>
2023-12-15 18:40:47 +0100

    CalendarSlot and WorkingDay as entities with logic

---
+++ src/CalendarAnalyser/CalendarAnalyser.Core/CalendarSlot.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CalendarAnalyser.Core;
+
+internal class CalendarSlot(TimeOnly startTime, TimeSpan length)
+{
+    public TimeOnly StartTime { get; } = startTime;
+    public TimeOnly EndTime { get; } = startTime.Add(length);    
+    private readonly ICollection<string> categories = new[] { Constants.FreeCategoryName };
+
+    public bool IsMeetingInSlot(TimeOnly meetingStart, TimeOnly meetingEnd) => (StartTime < meetingStart && EndTime > meetingStart) ||
+        (StartTime < meetingEnd && EndTime > meetingStart) || (StartTime > meetingStart && StartTime < meetingEnd && EndTime < meetingEnd && EndTime > meetingStart);
+
+    public void AddCategory(string categoryName)
+    {
+        if (categories.Count == 1 && categories.First() == Constants.FreeCategoryName)
+        {
+            categories.Clear();
+        }
+
+        categories.Add(categoryName);
+    }
+}
+    
---
+++ src/CalendarAnalyser/CalendarAnalyser.Core/WorkingDay.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CalendarAnalyser.Core;
+
+internal class WorkingDay(DateOnly date, TimeSpan slotResolution, TimeSpan coreHoursStart, TimeSpan coreHoursEnd)
+{
+    public DateOnly Date { get; } = date;
+    private readonly ICollection<CalendarSlot> slots = BuildSlots(slotResolution, coreHoursStart, coreHoursEnd);
+    public TimeSpan WorkingTime => slots.Count * slotResolution;
+
+    private static ICollection<CalendarSlot> BuildSlots(TimeSpan timeResolution, TimeSpan coreHoursStartTime, TimeSpan coreHoursEndTime)
+    {
+        var coreHoursLength = coreHoursEndTime - coreHoursStartTime;
+        var slotsPerDay = (int)Math.Ceiling(coreHoursLength.TotalMinutes / timeResolution.TotalMinutes);
+
+        var slots = Enumerable.Range(0, slotsPerDay).Select(i => new CalendarSlot(TimeOnly.FromTimeSpan(coreHoursStartTime.Add(TimeSpan.FromMinutes(i * timeResolution.TotalMinutes))), timeResolution)).ToArray();
+
+        return slots;
+    }
+
+    public void AddMeetingWithCategory(Meeting meeting, string category)
+    {        
+        if(DateOnly.FromDateTime(meeting.StartDateTime.Date) != Date || DateOnly.FromDateTime(meeting.EndDateTime.Date) != Date)
+        {
+            throw new InvalidOperationException($"Provided meeting is outside given working day - {Date}");
+        }
+
+        var meetingStartTime = TimeOnly.FromDateTime(meeting.StartDateTime);
+        var meetingEndTime = TimeOnly.FromDateTime(meeting.EndDateTime);
+
+        foreach (var slot in slots)
+        {
+            if(slot.IsMeetingInSlot(meetingStartTime, meetingEndTime))
+            {
+                slot.AddCategory(category);
+            }
+        }
+    }
+}
+    
