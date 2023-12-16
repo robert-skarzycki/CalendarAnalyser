76623219d809c44e3fc07df58f934cd9cba3d96c
Robert <3417357+robert-skarzycki@users.noreply.github.com>
2023-12-16 21:40:05 +0100

    MeetingsCategoryMatcherTests rebuilt after sut change

---
+++ src/CalendarAnalyser/CalendarAnalyser.Core.Tests/MeetingsCategoryMatcherTests.cs
@@ -0,0 +1,37 @@
+﻿using CalendarAnalyser.Core.Configuration;
+using FluentAssertions;
+using System.Text.RegularExpressions;
+
+namespace CalendarAnalyser.Core.Tests;
+
+public class MeetingsCategoryMatcherTests
+{
+    private readonly CalendarAnalysisConfiguration configuration;
+    private const string DummyCategoryName = "DummyCategory";
+
+    public MeetingsCategoryMatcherTests()
+    {
+        configuration = new CalendarAnalysisConfiguration
+        {
+            Rules = new IAnalysisRule[] {
+                new RegexAnalysisRule(new Regex("Dummy.*"), DummyCategoryName)
+            }
+        };
+    }
+
+    [Fact]
+    public void WhenRuleMatches_ReturnsCategoryName()
+    {
+        var result = MeetingsCategoryMatcher.Match(new DummyMeeting("DummyMeeting"), configuration);
+
+        result.Should().Be(DummyCategoryName);        
+    }
+
+    [Fact]
+    public void WhenNoRuleMatches_ReturnsOtherCategoryName()
+    {
+        var result = MeetingsCategoryMatcher.Match(new DummyMeeting("SomeMeeting"), configuration);
+
+        result.Should().Be(Constants.OtherCategoryName);
+    }
+}
